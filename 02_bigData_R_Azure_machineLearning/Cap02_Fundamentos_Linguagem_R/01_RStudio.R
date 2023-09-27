a5e728ff8cc06dec59cc0ae38756e23724c599b6
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2023-09-27 16:47:39 -0300

    Start Linguagem R

---
+++ 02_bigData_R_Azure_machineLearning/Cap02_Fundamentos_Linguagem_R/01_RStudio.R
@@ -0,0 +1,15 @@
+# Primeiros passos com R
+
+getwd() # Visualiza diretório
+
+license() # licença
+
+sessionInfo() # Resumo sobre a sessão
+
+print('Estou iniciando os estudos com a linguagem R')
+
+# Criando gráfico
+plot(1:25)
+
+# Instalando pacotes
+install.packages()
