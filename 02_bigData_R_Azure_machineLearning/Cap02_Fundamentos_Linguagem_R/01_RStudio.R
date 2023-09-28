f96a25d95fa16e931cc20404d44feb8471a6fb3d
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2023-09-28 09:13:50 -0300

    update - ideias iniciais do R

--- 02_bigData_R_Azure_machineLearning/Cap02_Fundamentos_Linguagem_R/01_RStudio.R
+++ 02_bigData_R_Azure_machineLearning/Cap02_Fundamentos_Linguagem_R/01_RStudio.R
@@ -12,4 +12,33 @@ print('Estou iniciando os estudos com a linguagem R')
 plot(1:25)
 
 # Instalando pacotes
-install.packages()
+install.packages('randomForest')
+install.packages('ggplot2')
+install.packages('dplyr')
+install.packages('devtools')
+
+# Carregando pacote da memória
+library(ggplot2)
+
+# Descarregar o pacote 
+detach(package:ggplot2)
+
+# Se souber o nome da função 
+help(sum)
+?sum
+
+# Para buscar mais opçoes sobre uma função, podemos utilizar o SOS
+install.packages("sos")
+library(sos)
+findFn("fread")
+
+# Se souber o nome da função
+help.search('randomForest')
+help.search('matplot')
+??matplot # Outra forma de chamar o help
+RSiteSearch('matplot')
+example('matplot') # Exemplo de utilização da função matplot
+
+
+# Sair
+q()
