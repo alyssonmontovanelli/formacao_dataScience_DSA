8439f1b76b62db115a2ae0fe198c0466c3e36cbd
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2024-01-08 19:30:39 -0300

    Voltando ao foco

---
+++ 02_bigData_R_Azure_machineLearning/Cap02_Fundamentos_Linguagem_R/08_Listas.R
@@ -0,0 +1,45 @@
+
+# ------------------ LISTAS -----------------------------# 
+
+# listas de String
+lista1 = list('A', 'B','C')
+lista1
+lista2 <- list(c("A", "A"), "b", 'c')
+lista2
+lista3 = list(matrix(c("A", "B", "C", "D"), nr = 2), "T", "V")
+lista3
+
+
+# Lista inteiros
+lista_inteiros = list(2,3,4)
+lista_inteiros
+
+# Lista de floats
+lista_float = list(1.3, 4.4, 5.5)
+lista_float
+
+
+#lista valores lógicos
+lista_bol <- list(TRUE, TRUE, FALSE, FALSE)
+lista_bol
+
+
+
+# Lista composta
+
+composta = list(1,'A',TRUE)
+composta
+
+
+
+# LISTA NORMALIZADA (Distribuição normal dos valores)
+lista4 <- list(1:10, c("A", 'B'), rnorm(10))
+lista4
+
+
+# slicing
+lista4[1]
+lista4[2]
+lista4[[2]][1]
+lista4[[2]][1] = 'Alysson'
+lista4
