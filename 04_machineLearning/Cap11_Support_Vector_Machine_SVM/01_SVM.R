893114514a12fd51950c4572dc8d338ac04ee9cd
Alysson Montovanelli <alysson.montovanelli@gmail.com>
2024-09-23 13:06:02 -0300

    SVM_R_estudo_de_caso

--- 04_machineLearning/Cap11_Support_Vector_Machine_SVM/01_SVM.R
+++
@@ -1,4 +0,0 @@
-setwd("C:/formacao_dataScience_DSA/04_machineLearning/Cap11_Support_Vector_Machine_SVM")
-getwd()
-
-pitoria <- 123
---
+++ 04_machineLearning/Cap11_Support_Vector_Machine_SVM/02_SVM_pre_processing_1.R
@@ -0,0 +1,12 @@
+getwd()
+
+# continuação do Script anterior - PRÉ PROCESSAMENTO DOS DADOS
+
+library(dplyr)
+library(caret)
+library(gains) # interpreta o modelo SVM
+library(pROC) # Curva ROC
+library(ROCR) # metrica 
+library(ROSE) # manipulação de dado
+library(e1071) # SVM
+library(mice) # limpeza de dados
